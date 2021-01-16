717d2697d894ccaab07d199c95b52d7335c18663
Kerrie <wongkerrie@gmail.com>
2021-01-16 20:33:50 +0000

    initial commit
    Co-authored- by: Amina B <amina.bretmo@gmail.com>

---
+++ game.py
@@ -0,0 +1,17 @@
+"""A number-guessing game."""
+
+print("hi")
+
+def get_user_name():
+    """Returns user name."""
+    name = input("What is your name?")
+    return name
+
+get_user_name
+
+import random
+min = 0
+max = 100
+print(f"{name}, I'm thinking of a number between {min} and {max}")
+print("Try to guess my number.")
+
