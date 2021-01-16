181300dcc88715ee4dc41d1d7687e3eff8a616b6
Kerrie <wongkerrie@gmail.com>
2021-01-16 20:36:03 +0000

    Display player greating

--- game.py
+++ game.py
@@ -12,6 +12,6 @@ get_user_name
 import random
 min = 0
 max = 100
-print(f"{name}, I'm thinking of a number between {min} and {max}")
+print(f"{get_user_name}, I'm thinking of a number between {min} and {max}")
 print("Try to guess my number.")
 
